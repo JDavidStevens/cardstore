7fe5f6f7bb1cee7affae0b3af2173dc410f4510a
unknown <jdavid.stevens@gmail.com>
2018-08-01 13:28:26 -0600

    cs81d

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,40 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Shop extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/product').then(response => {
+      this.setState({
+        products: response.data
+      });
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-        <h3>Card</h3>
-        <img />
-        <button>Add to Cart</button>
-      </div>
-    );
+    let productDisplay = this.state.products.map((element, index) => {
+      return (
+        <div className="product-container" key={index}>
+          <h2>{element.name}</h2>
+          <img src={element.image} alt="" />
+          <h3>{'$' + element.price}</h3>
+          <button onClick={() => this.props.addToShoppingCart(element)}>
+            Purchase!
+          </button>
+        </div>
+      );
+    });
+    return;
+    <div className="storefront-container">{productDisplay}</div>;
   }
 }
