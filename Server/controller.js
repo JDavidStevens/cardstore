a2b4eed575818b7360dfb70b9cb17a785b9707b8
unknown <jdavid.stevens@gmail.com>
2018-08-02 11:25:42 -0600

    cs82a

--- Server/controller.js
+++ Server/controller.js
@@ -59,5 +59,40 @@ module.exports = {
         res.status(500).send({ errorMessage: 'Oops! Something went wrong.' });
         console.log(err);
       });
+  },
+  create: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    const { product_name, image, price } = req.body;
+
+    dbInstance
+      .create_customer([first_name, last_name, email])
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: 'Oops! Something went wrong.' });
+        console.log(err);
+      });
+  },
+  getOne: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    const { params } = req;
+
+    dbInstance
+      .read_customer(params.id)
+      .then(product => res.status(200).send(product))
+      .catch(err => {
+        res.status(500).send({ errorMessage: 'Oops! Something went wrong.' });
+        console.log(err);
+      });
+  },
+  getAll: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+
+    dbInstance
+      .read_customers()
+      .then(products => res.status(200).send(products))
+      .catch(err => {
+        res.status(500).send({ errorMessage: 'Oops! Something went wrong.' });
+        console.log(err);
+      });
   }
 };
--- Server/server.js
+++ Server/server.js
@@ -2,19 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
 const massive = require('massive');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const{
+const {
   REACT_APP_DOMAIN,
   REACT_APP_CLIENT_ID,
   CLIENT_SECRET,
   SESSION_SECRET,
   CONNECTION_STRING
-}
+} = process.env;
 
 massive(CONNECTION_STRING)
   .then(dbInstance => {
@@ -22,67 +23,63 @@ massive(CONNECTION_STRING)
   })
   .catch(err => console.log(err));
 
- 
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-  // app.use(
-  //   session({
-  //     secret: SESSION_SECRET,
-  //     resave: false,
-  //     saveUninitialized: false
-  //   }) 
-  // )
+app.get('/auth/callback', async (req, res) => {
+  let payload = {
+    client_id: REACT_APP_CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    code: req.query.code,
+    grant_type: 'authorization_code',
+    redirect_uri: `http://${req.headers.host}/auth/callback`
+  };
 
-  // app.get('/auth/callback', async (req, res) => {
-    
-  //   let payload = {
-  //     client_id: REACT_APP_CLIENT_ID,
-  //     client_secret: CLIENT_SECRET,
-  //     code: req.query.code,
-  //     grant_type: 'authorization_code',
-  //     redirect_uri: `http://${req.headers.host}/auth/callback`
-  //   };
-  
-    
-  //   let resWithToken = await axios.post(
-  //     `https://${REACT_APP_DOMAIN}/oauth/token`,
-  //     payload
-  //   );
-    
-  
-  //   let resWithUserData = await axios.get(
-  //     `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
-  //       resWithToken.data.access_token
-  //     }`
-  //   );
-  
-  //   const db = req.app.get('db');
-  //   let { sub, email, name, picture } = resWithUserData.data;
-  //   let foundUser = await db.find_user([sub]);
-  //   if (foundUser[0]) {
-      
-  //     req.session.user = foundUser[0];
-      
-  //     res.redirect('/#/cart');
-  //   } else {
-      
-  //     let createdUser = await db.create_user([name, email, sub, picture]);
-      
-  //     req.session.user = createdUser[0];
-  //     res.redirect('/#/cart');
-  //   }
-  // });
-  
-  // app.get('/api/user-data', (req, res) => {
-  //   if (req.session.user) {
-  //     res.status(200).send(req.session.user);
-  //   } else {
-  //     res.status(401).send('Nice try!');
-  //   }
-  // });
+  let resWithToken = await axios.post(
+    `https://${REACT_APP_DOMAIN}/oauth/token`,
+    payload
+  );
+
+  let resWithUserData = await axios.get(
+    `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
+      resWithToken.data.access_token
+    }`
+  );
+
+  const db = req.app.get('db');
+  let { first_name, last_name, email } = resWithUserData.data;
+  let foundUser = await db.find_user([sub]);
+  if (foundUser[0]) {
+    req.session.user = foundUser[0];
+
+    res.redirect('/#/cart');
+  } else {
+    let createdUser = await db.create_customer([first_name, last_name, email]);
+
+    req.session.user = createdUser[0];
+    res.redirect('/#/cart');
+  }
+});
+
+app.get('/api/user-data', (req, res) => {
+  if (req.session.user) {
+    res.status(200).send(req.session.user);
+  } else {
+    res.status(401).send('Nice try!');
+  }
+});
 
 app.post('/api/product', controller.create);
+app.post('/api/customer', controller.create);
 app.get('/api/products', controller.getAll);
 app.get('/api/product/:id', controller.getOne);
+app.get('/api/customers', controller.getOne);
+app.get('/api/customer/:id', controller.getOne);
 app.put('/api/product/:id', controller.update);
 app.delete('/api/product/:id', controller.delete);
 
