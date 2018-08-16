be90e0a8a5ad0f5c03f3b4e5d5b7f26dcc6e3c4d
unknown <jdavid.stevens@gmail.com>
2018-08-16 12:37:18 -0600

    cs816c_sld_shw_strt

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -24,8 +24,16 @@ export default class Home extends Component {
     let productDisplay = this.state.products.map((element, index) => {
       return <img src={element.picture} alt="" className="pic" />;
     });
+
+    const Slideshow = () => {
+      return (
+        <Slide images={productDisplay} duration={5000} transition={1000} />
+      );
+    };
+
     return (
       <div className="home-landing">
+        {productDisplay}
         Like What You See? Login to Make a Purchase
       </div>
     );
