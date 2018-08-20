0594c6f5db6f1526ad255bb2a05b04f21f2e8a83
unknown <jdavid.stevens@gmail.com>
2018-08-20 10:55:01 -0600

    csAuth0fix

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -24,7 +24,7 @@ export default class Home extends Component {
 
   login() {
     let { REACT_APP_DOMAIN, REACT_APP_CLIENT_ID } = process.env;
-    let url = `${window.location.origin}/auth/callback`;
+    let url = `${window.location}/auth/callback`;
 
     window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`;
   }
